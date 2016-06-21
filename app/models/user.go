92d6bd78d8fad797b4cb846cf5d6ba1c37cbf047
Carl Tablante <carl.q.tablante@gmail.com>
2016-06-21 08:04:08 +0800

    iterating a slice of maps and display in views

--- app/models/user.go
+++ app/models/user.go
@@ -22,16 +22,19 @@ func (u *User) All() *sql.Rows{
   return stmnt
 }
 
-func All() (users map[string]string){
-  var (
-    uname string
-    email string
-    )
+func All() []map[string]interface{}{
+  var username, email string
 
-  rows, _ := app.DB.Query("Select username, email from users where id = 1")
+  users := make([]map[string]interface{}, 2)
+
+  i := 0
+  rows, _ := app.DB.Query("Select username, email from users")
+
   for rows.Next() {
-    rows.Scan(&uname, &email)
-    revel.INFO.Println(email)
+    rows.Scan(&username, &email)
+    users[i] = map[string]interface{}{"username": username, "email": email}
+    i++
+    revel.INFO.Println(users[0]["email"])
   } 
 
   return users
